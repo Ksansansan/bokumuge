b2e9f1753d9cf1bd351817a6108470e094224920
Ksansansan <131240508+Ksansansan@users.noreply.github.com>
2026-03-21 11:08:53 +0000

    raid test go

--- src/raid/raidManager.js
+++ src/raid/raidManager.js
@@ -10,8 +10,8 @@ let playerRef = null;
 let currentRaidData = null;
 let countdownInterval = null;
 
-const RAID_HOURS =[0, 3, 6, 9, 12, 15, 18, 21];
-const RAID_DURATION_MINUTES = 30;
+const RAID_HOURS =[0, 3, 6, 9, 12, 15, 18,20, 21];
+const RAID_DURATION_MINUTES = 15;
 
 export function initRaidManager(playerObj) {
   playerRef = playerObj;
